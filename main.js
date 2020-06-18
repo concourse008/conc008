7a09489bb0a14edb81c3da5c5fc53d19b3c1d694
concourse008 <61238161+concourse008@users.noreply.github.com>
2020-06-18 21:34:52 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,163 +1,250 @@
 'use strict';
 const game0Divided = document.getElementById('game-area0');
 const game1Divided = document.getElementById('game-area1');
-let talk = '初期';//これを表示する文字の格納先とする
-let nowID = 0;//現在のイベントidを記録する
-let status = [0,100,100,5];//ハズレ、元気、たいまつ、100円
+let talk = '初期';  //表示するイベントを格納
+let place = 0;     //現在の場所を記録
+let flag = [];      //初回のみのイベントのフラグをまとめる
+
+let mitokae = {
+  x: 160,
+  y: 100
+}
 
 //画像読み込み
-  const canvas = {
-    0: document.getElementById("canvas1"),
-    1: document.getElementById("canvas2"),
-    2: document.getElementById("canvas0")//入力用
-    };
-  let flip = 0;
-  canvas[1-flip].style.visibility='hidden';
-  canvas[flip].style.visibility='visible';
-  canvas[2].style.visibility='visible';
-  flip = 1 - flip;
-  let ctx = canvas[flip].getContext('2d');
-  const ctx0 = canvas[2].getContext('2d');
-  const srcs = [//画像一覧
-    ['back.png',0,0],
-    ['yes.png',205,270],
-    ['no.png',10,270],
-    ['message.png',10,185],
-    ['mito2.png',50,100]
-  ];
-  let images = [];
-  for (let i in srcs){
-    images[i] = new Image();
-    images[i].src = srcs[i][0];
-  }
-  let loadedCount = 1;
-  for (let i in images){
-    images[i].addEventListener('load',function(){
-      if (loadedCount == images.length){
-        for(let j in images){
-          ctx.drawImage(images[j],srcs[j][1],srcs[j][2]);
-          window.requestAnimationFrame(step);
-        }
+const canvas = {
+  0: document.getElementById("canvas1"),
+  1: document.getElementById("canvas2"),
+  2: document.getElementById("canvas0")//入力用
+};
+let flip = 0;
+canvas[1 - flip].style.visibility = 'hidden';
+canvas[flip].style.visibility = 'visible';
+canvas[2].style.visibility = 'visible';
+flip = 1 - flip;
+let ctx = canvas[flip].getContext('2d');
+const ctx0 = canvas[2].getContext('2d');
+const srcs = [//画像一覧
+  ['pic3.png', 205, 333],
+  ['pic2.png', 10, 333],
+  ['pic1.png', 205, 270],
+  ['pic0.png', 10, 270],
+  ['message.png', 10, 185],
+  ['mito2.png', 160, 90]
+];
+const srcs2 = [//背景画像一覧
+  ['back0.png', 0, -5],
+  ['back1.png', 0, -5],
+  ['back.png', 0, 0]
+];
+let images = [];
+for (let i in srcs) {
+  images[i] = new Image();
+  images[i].src = srcs[i][0];
+}
+let loadedCount = 1;
+for (let i in images) {
+  images[i].addEventListener('load', function () {
+    if (loadedCount == images.length) {
+      for (let j in images) {
+        ctx.drawImage(images[j], srcs[j][1], srcs[j][2]);
+
       }
-      loadedCount++;
-    },false);
-  }
+    }
+    loadedCount++;
+  }, false);
+}
+let backimages = [];
+for (let i in srcs2) {
+  backimages[i] = new Image();
+  backimages[i].src = srcs2[i][0];
+}
+let loadedCount2 = 1;
+for (let i in backimages) {
+  backimages[i].addEventListener('load', function () {
+    if (loadedCount2 == backimages.length) {
+      for (let j in backimages) {
+        ctx.drawImage(backimages[j], srcs2[j][1], srcs2[j][2]);
+        console.log('a');
+        window.requestAnimationFrame(step);
+      }
+    }
+    loadedCount2++;
+  }, false);
+}
 //画像読み込み
 
 //画面の描写
-  function step(){
-    window.requestAnimationFrame(step);
-    canvas[1-flip].style.visibility='hidden';
-    canvas[flip].style.visibility='visible';
-    flip = 1 - flip;
-    ctx = canvas[flip].getContext('2d');
-    for(let j in images){
-      ctx.drawImage(images[j],srcs[j][1],srcs[j][2]);
-    }
-    //ステータスの表示
-    ctx.font = "16px sans-serif"
-    let sta = 'やるき：'+status[1]+'％　たいまつ：'+status[2]+'％　100円玉：'+status[3]+'枚';
-    ctx.fillText(sta,5,22)
-    //文字の表示
-    for (let lines= (String(talk.value)).split("\n"),i=0,l=lines.length;l>i;i++){
-      ctx.font = "18px sans-serif";
-      let line = lines[i];
-      let addY = 18;
-      if (i) addY += 18 * 1.16 * i ;
-      ctx.fillText(line,50,190+addY);
-    }
-    //選択肢の表示
-    if (talk instanceof Branch){
-    for (let lines= (String(talk.choice0)).split("\n"),i=0,l=lines.length;l>i;i++){
-      ctx.font = "16px sans-serif";
-      let line = lines[i];
-      let addY = 18;
-      if (i) addY += 18 * 1.16 * i ;
-      ctx.fillText(line,30,290+addY);
-    }
-    for (let lines= (String(talk.choice1)).split("\n"),i=0,l=lines.length;l>i;i++){
-      ctx.font = "16px sans-serif";
-      let line = lines[i];
-      let addY = 18;
-      if (i) addY += 18 * 1.16 * i ;
-      ctx.fillText(line,230,290+addY);
-    }
+let oldbi = 400;
+let nowbi = 0;
+let oldplace = 0;
+function step() {
+  window.requestAnimationFrame(step);
+  canvas[1 - flip].style.visibility = 'hidden';
+  canvas[flip].style.visibility = 'visible';
+  flip = 1 - flip;
+  ctx = canvas[flip].getContext('2d');
+  ctx.clearRect(0, 0, 400, 400);
+  ctx.drawImage(backimages[srcs2.length-1], srcs2[srcs2.length-1][1], srcs2[srcs2.length-1][2]);//ベース
+  ctx.drawImage(backimages[oldplace], oldbi, -5);//場面背景古い
+  ctx.drawImage(backimages[place], nowbi, -5);//場面背景新しい
+  for (let j in images) {
+    ctx.drawImage(images[j], srcs[j][1], srcs[j][2]);
   }
-    //ほぼ60fpsで繰り返すそう、本当か？
-    console.log('a');
+  //文字の表示
+  for (let lines = (String(talk.value)).split("\n"), i = 0, l = lines.length; l > i; i++) {
+    ctx.font = "18px sans-serif";
+    let line = lines[i];
+    let addY = 18;
+    if (i == 1) {
+      addY += 18 * 1.46 * i
+    } else {
+      addY += 18 * 1.26 * i
+    }
+    ctx.fillText(line, 50, 190 + addY);
   }
+  //選択肢の表示
+  if (talk instanceof Branch || talk instanceof Menu) {
+    ctx.font = "20px sans-serif";
+    ctx.fillText(talk.choice0, 30, 305);
+    ctx.fillText(talk.choice1, 230, 305);
+    ctx.fillText(talk.choice2, 30, 367);
+    ctx.fillText(talk.choice3, 230, 367);
+  }
+  //ほぼ60fpsで繰り返すそう、本当か？
+  console.log('a');
+}
 //画面の描写
 
 //クリック
 let point = 0;
-canvas[2].addEventListener('click',e =>{
+canvas[2].addEventListener('click', e => {
   //マウスの座標をカンバス内の座標と合わせる
   const rect = canvas[2].getBoundingClientRect();
   point = {
     x: e.clientX - rect.left,
     y: e.clientY - rect.top
   }
-  if (talk instanceof Last){
+  if (talk instanceof Last) {
     status[talk.plusid] = status[talk.plusid] + talk.plusv;
-    status[talk.minusid] = status[talk.minusid] + talk.minusv;
+    status[talk.minusid] = status[talk.minusid] - talk.minusv;
   }
-  if (talk.next0 == 0){
-    startgame();
-  }else if (talk instanceof Branch){
-    if(point.x >= 50 && point.x <= 170 && point.y >= 270&&point.y <= 390){
-    talk = talk.next0;
-    } else if(point.x >= 230 && point.x <= 350 && point.y >= 270&&point.y <= 390){
-    talk = talk.next1;
-    }
-  }else{
+  if (talk instanceof Branch) {
+    branchdo();
+  } else if (talk instanceof Menu) {
+    menudo();
+  } else {
     talk = talk.next0;
   }
 })
 //クリック
 
 //テキストのみ
-class Text{
-  constructor(value,next0){
+class Text {
+  constructor(value, next0) {
     this.value = value;
     this.next0 = next0;
   }
 }
-//選択肢あり
-class Branch extends Text{
-  constructor(value,next0,next1,choice0,choice1){
-    super(value,next0);
+//調べる
+class Branch extends Text {
+  constructor(value, next0, next1, next2, next3, choice0, choice1, choice2, choice3) {
+    super(value, next0);
     this.next1 = next1;
+    this.next2 = next2;
+    this.next3 = next3;
     this.choice0 = choice0;
     this.choice1 = choice1;
+    this.choice2 = choice2;
+    this.choice3 = choice3;
+  }
+}
+function branchdo(){
+  if (point.x >= 50 && point.x <= 170 && point.y >= 270 && point.y <= 327) {
+    talk = talk.next0;
+  } else if (point.x >= 230 && point.x <= 350 && point.y >= 270 && point.y <= 327) {
+    talk = talk.next1;
+  }else if (point.x >= 50 && point.x <= 170 && point.y >= 333 && point.y <= 390) {
+    talk = talk.next2;
+  }else if (point.x >= 230 && point.x <= 350 && point.y >= 333 && point.y <= 390) {
+    talk = talk.next3;
+  }
+}
+//メニュー
+class Menu extends Text {
+  constructor(value) {
+    super(value);
+    this.choice0 = '左に進む';
+    this.choice1 = '右に進む';
+    this.choice2 = 'アイテム';
+    this.choice3 = 'しらべる';
+  }
+}
+let rightgo = 0;
+let leftgo = 0;
+function menudo(){
+  if (point.x >= 10 && point.x <= 195 && point.y >= 270 && point.y <= 327) {
+    oldplace = place;
+    place = place -1;
+    oldbi = 0;
+    nowbi = -400;
+    ju();
+    leftgo = setInterval(left, 1000/30);
+  }else if (point.x >= 205 && point.x <= 390 && point.y >= 270 && point.y <= 327) {
+    oldplace = place;
+    place = place +1;
+    oldbi = 0;
+    nowbi = 400;
+    ju();
+    rightgo = setInterval(right, 1000/30);
+  }else if (point.x >= 10 && point.x <= 195 && point.y >= 333 && point.y <= 390) {
+    console.log('2');
+  }else if (point.x >= 205 && point.x <= 390 && point.y >= 333 && point.y <= 390) {
+    console.log('3');
+  }
+}
+function right(){
+  oldbi = oldbi -8;
+  nowbi = nowbi -8;
+  if (nowbi == 0){
+    clearInterval(rightgo);
+    clearInterval(jumping);
+  }
+}
+function left(){
+  oldbi = oldbi +8;
+  nowbi = nowbi +8;
+  if (nowbi == 0){
+    clearInterval(leftgo);
+    clearInterval(jumping);
   }
 }
 //ステータス処理あり
-class Last extends Text{
-  constructor(value,next0,plusid,plusv,minusid,minusv){
-    super(value,next0);
-    this.plusid = plusid;
-    this.plusv = plusv;
-    this.minusid = minusid;
-    this.minusv = minusv;
+class Last extends Text {
+  constructor(value, next0, itemid) {
+    super(value, next0);
+    this.itemid = itemid;
   }
 }
 
-//イベント抽選
-let idol = new Text('行動中……',0);
-function startgame(){
-  let i = Math.floor(Math.random()*events.length);
-  status[2] = status[2] -10;
-  talk = events[i] ;
+
+const menu0 = new Menu('＊カエデ\nたのしみやなぁ……');
+const op6 = new Text('＊カエデ\nうん、行こ！', menu0);
+const op5 = new Text('＊ミト\n残ってる物があるかもしれませんね\n行ってみましょうか？', op6);
+const op4 = new Text('＊カエデ\nオゾン！ なんかええなぁ\nよう燃えそうな感じするわ', op5);
+const op3 = new Text('＊ミト\n色んなものを売っていたお店です\nあれは『OZONE』ってお店ですよ', op4);
+const op2 = new Text('＊カエデ\nなんなんそれ？', op3);
+const op1 = new Text('＊ミト\nあれは……\nショッピングモールですねぇ', op2);
+const op0 = new Text('＊カエデ\nミトちゃん\nあのでっかいのなんやろ？', op1);
+
+talk = op0;
+
+
+let jumpi = 0;
+function jump() {
+  jumpi = 1 - jumpi;
+  srcs[srcs.length - 1][2] = 90 - 3 * jumpi;
 }
-
-let juce3 = new Last('「ガマンや！」\nやる気が 10 あがった！',idol,1,10,0,0);
-let juce2 = new Last('「おいしい！」\n100円を 1 失った！\nやる気が 20 あがった！',idol,1,20,3,-1);
-let juce1 = new Branch('自販機でジュースを買う？',juce3,juce2,'のまない','のむ');
-let juce0 = new Text('カエデは自動販売機を\n見つけた！',juce1);
-
-let mito1 = new Last('「ほんまに？ やったー！」\nやる気が 20 あがった！',idol,1,20,0,0);
-let mito0 = new Text('カエデはミトと出会った！\n「カエデちゃんおつかいですか？\nえらいですねぇ……」',mito1);
-
-const events = [juce0,mito0];
-
-talk = idol;
+let jumping = 0;
+function ju(){
+  jumping = setInterval(jump, 200);
+}
